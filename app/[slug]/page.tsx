610e0f7813a6f9d22584836f0d81d40ae4cce5ce
root <root@DESKTOP-ATFCIID>
2024-01-28 20:06:05 -0400

    agregando info contact y about

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -10,7 +10,9 @@ export default async function Page({ params }: Props) {
   const page = await getPage(slug)
   return (
     <div>
-      <h1 className="ml-6 drop-shadow text-3xl font-bold">{page.title}</h1>
+      <h1 className="text-center  drop-shadow text-3xl font-bold">
+        {page.title}
+      </h1>
 
       <div className="max-w-3xl text-center mt-12 text-xl mx-auto ">
         <PortableText value={page.content} />
